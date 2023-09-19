9ffdcefe325fa2c95e108735ee465f872ff90802
Terry <terrywan117@gmail.com>
2023-09-19 12:51:12 -0400

    Implementing my card

--- src/App.js
+++ src/App.js
@@ -1,21 +1,35 @@
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
 
 
     return (
-      <div className="App">
-        <h1>Hello World!</h1>
-        <h1>Welcome to my card</h1>
-        <img src="https://cdn.creatureandcoagency.com/uploads/2021/03/FrogLifecycle_Body_8.png" alt="Frog" />
-        <div className="paragraph">
-          <p>This is a picture of a frog that I found on the internet. It looks really cool, and I thought it would be nice to share.</p>
-        </div>
-        <div class="btn-wrapper">
- <a href="https://hax.psu.edu">
-   <button>Details</button> </a> </div>
+      <div class="wrapper">
+
+      <div class="card" id="card">
+
+      <h1>super</h1>
+        
+      <img src="https://www.gamersdecide.com/sites/default/files/authors/u156421/super.jpg" alt="Card Image" />
+        
+        <details>
+          <summary>Description</summary>
+          <p>Matthew "super" DeLisi is a retired American player who last played for San Francisco Shock. He is one of the most decorated competitive Overwatch players, being a two time Overwatch League Champion, World Cup Champion, Stage 2 Champion, May Melee Tournament Champion, Role Star, All Star and MVP Runner-Up.</p>
+        </details>
+        
       </div>
+        
+      <Button class="button-other" id="button-duplicate">Duplicate</Button>
+        
+      <Button class="button-other" id="button-delete">Delete</Button>
+        
+      <Button class="button-other" id="button-color" onClick="toggleBackground()">Toggle Background Color</Button>
+
+      <Button class="button-other" id="button-heading" onClick="changeHeading()">Change Heading</Button>
+        
+      <Button class="button-other" id="button-description" onClick="toggleDescription()">Toggle Description</Button>
+        
+      </div>  
     );
   }
 
